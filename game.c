362d75143d0139ac46e30a4cade2b3dc6654bad3
MathieuRund <mattis.rund@hotmail.com>
2017-04-09 14:43:11 -0400

    game update

--- game.c
+++ game.c
@@ -1,47 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
 #include <string.h>
 
+void getInput(char *dest, int *i){
+    char c = getchar();
+    //cut everything out until "="
+    for(;c!='=';(*i)++)
+        c = getchar();
+    //give dest all info until "&"
+    int j = *i;
+    for(;c!='&'&&c!='\0';i++){
+        dest[*i-j] = c;
+        c = getchar();
+    }
+    dest[*i-j] = '\0';
+}
+
 void main ( int argc, char *argv[] ) {
-	int poemLength;
-	int playerGold;
-	int playerMana;
-	int roomGold, roomMana;
-	FILE *resources = fopen("resources.csv", "r");
-	int i = 0;
-	int j = 1;
-	char valuables[7];
-	while (!feof(resources)) {
-		char c = getc(resources);
-		temp[i] = c;
-		if (c == ',') {
-			valuables[i] = '\0';
-			if (j == 2) {
-				roomMana = atoi(valuables);	
-			} 
-			else if (j == 1) {
-				roomGold = atoi(valuables);
-			}
-			j++;
-			i = 0;
-		} 
-		else  i++; 
-	}
-	fclose(ressources);
-	char *poem;
+    int i = 0;
+    int size = atoi(getenv("CONTENT_LENGTH"));
+    char *command = (char*)malloc(size);
+    getInput(command, &i);
+    char *poem = (char*)malloc(size);
+    getInput(poem, &i);
+	int poemLength = strlen(poem);
 	if ( poemLength <= 10 ) {
 		printf(" Although we do believe in consiseness, there is a limit to how short a beautiful poem can be.  ");
 	}
-	time_t t; 
+	time_t t;
   	/* Intializes random number generator */
    	srand((unsigned) time(&t));
-	printf("   Random numbers generated: \n");
+	printf("Random numbers generated: \n");
 	int r = rand()%10;
 	printf(" %d", r);
 	if (r == 9){
-		printf(" That was... truly beautiful! Take all your");	
+		printf(" That was... truly beautiful! Take all your");
 	}
 	else{
 		printf("Maybe you should ponder over your word choice a little more");
