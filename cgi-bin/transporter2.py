f451e723e42bda8e58da4223d68f2a11ea884990
Abhijay Gupta <AbhijayGupta@dhcp204025.rez.mcgill.ca>
2017-04-09 18:58:11 -0400

    some progress...

--- cgi-bin/transporter2.py
+++ cgi-bin/transporter2.py
@@ -8,28 +8,38 @@ def transport():
         userUrl = url[:-9]
     resourcesLoc = 'http://cs.mcgill.ca/~mrunds/Game/resources.csv'
     roomLoc = 'http://cs.mcgill.ca/~mrunds/Game/room.html'
-    input_file = open(resourcesLoc, "r")
-    details = input_file.readlines()
-    details = details[0]
-    details = details.replace(" ", "")
-    print details
-    resources = details.split(',')
-    occupiedOrNot = int (resources[2])
-    print occupiedOrNot
+    try:
+        input_file = open(resourcesLoc, "r")
+        details = input_file.readlines()
+        detailsLine1 = details[0]
+        detailsLine1 = details.replace(" ", "")
+        print detailsLine1
+        resources = detailsLine1.split(',')
+        occupiedOrNot = int (resources[2])
+        print occupiedOrNot
 
-    if occupiedOrNot == 1:
-        call([userUrl + "room.cgi"])
-        return
+        if occupiedOrNot == 1:
+            call([userUrl + "room.cgi"])
+            return
 
-    else:
-        html_file = urllib2.urlopen(roomLoc)
-        print "Content-type: text/html"
-        for line in html_file:
-            line = line + ''
-            if "inventory" not in line:
-                print line
-            else:
-                mana = mana + ''
-                gold = gold + ''
-                print '<input type="hidden" name="inventory" value="%s,%s"></input>' %mana %gold
+        else:
+            output_file = open(resourcesLoc, "w")
+            output_file.write(resources[0] + "," + resources[1] + "," + '0')
+            html_file = urllib2.urlopen(roomLoc)
+            print "Content-type: text/html"
+            for line in html_file:
+                line = line + ''
+                if "inventory" not in line:
+                    print line
+                else:
+                    mana = mana + ''
+                    gold = gold + ''
+                    print '<input type="hidden" name="inventory" value="%s,%s"></input>' %mana %gold
+    
+    except IOError:
+        print 'Cannot open file'
         # call(["./room.cgi"])
+    
+    finally:
+        input_file.close()
+        output_file.close()
