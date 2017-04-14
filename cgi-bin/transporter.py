28b8f8b33d99244e1a7134236e65d611bace87c2
atg-abhijay <atg.abhijay@gmail.com>
2017-04-13 23:09:14 -0400

    Add files via upload

--- cgi-bin/transporter.py
+++ cgi-bin/transporter.py
@@ -2,28 +2,33 @@ import cgi, cgitb, urllib2
 from subprocess import call
 
 def transport():
+    """ From the form on the web page,
+        we get the player's inventory
+        and the url of the room that
+        they came from """
     form = cgi.FieldStorage()
     inventoryVals = form.getvalue('inventory')
     url = form.getvalue('url')
     # url = 'http://www.cs.mcgill.ca/~sli144/room.html'
-    # print inventoryVals
     inventoryVals = inventoryVals + ''
     inventoryAsList = inventoryVals.split(',')
     mana = inventoryAsList[0]
     gold = inventoryAsList[1]
-    # mana = int (inventoryAsList[0])
-    # gold = int (inventoryAsList[1])
 
     usrUrl = ''
     if url.endswith('room.html'):
         userUrl = url[:-9]
     
-    # resourcesLoc = 'http://cs.mcgill.ca/~mrunds/Game/resources.csv'
-    # roomLoc = 'http://cs.mcgill.ca/~mrunds/Game/room.html'
+    """ resources.csv is stored in the
+        same place as transporter.py and
+        room.html is stored one level above """
     resourcesLoc = './resources.csv'
     roomLoc = '../room.html'
 
     try:
+        """ we access the resources file and
+            get the last number which tells us
+            if the room is occupied or not """
         input_file = open(resourcesLoc, "r")
         details = input_file.readlines()
         detailsLine1 = details[0]
@@ -33,11 +38,19 @@ def transport():
         occupiedOrNot = int (resources[2])
         # print occupiedOrNot
 
+        """ if the room is occupied we send
+            the player back to the room where
+            they came from by calling the program
+            on the previous website """
         if occupiedOrNot == 1:
             call([userUrl + "room.cgi"])
             return
 
         else:
+            """ if the room is not occupied, we
+                change our resources to reflect
+                that it is now occupied and we
+                print out the webpage """
             output_file = open(resourcesLoc, "w")
             output_file.write(resources[0] + "," + resources[1] + "," + '1')
             html_file = open(roomLoc, "r")
@@ -47,8 +60,6 @@ def transport():
                 if "inventory" not in line:
                     print line
                 else:
-                    # mana = mana + ''
-                    # gold = gold + ''
                     print '<input type="hidden" name="inventory" value="%s,%s"></input>' %mana %gold
     
     except IOError:
